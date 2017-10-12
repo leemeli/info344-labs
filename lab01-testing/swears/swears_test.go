2d893aae82a3ed82ca7b404ba6c1961b401e5404
Melody Lee <leemeli@uw.edu>
2017-10-12 16:10:40 -0700

    10/12

--- lab01-testing/swears/swears_test.go
+++ lab01-testing/swears/swears_test.go
@@ -2,18 +2,40 @@ package main
 
 import "testing"
 
-func TestcountSwears(t *testing.T) {
+func TestCountSwears(t *testing.T) {
+
+	cases := []struct {
+		name           string
+		input          []string
+		expectedOutput []SwearCounts
+	}{
+		{
+			name:           "empty KnownSwears",
+			input:          []string{},
+			expectedOutput: []SwearCounts{},
+		},
+		{
+			name:           "non-empty KnownSwears",
+			input:          []string{"Ha", "fuck", "bitch", "lol"},
+			expectedOutput: []SwearCounts{"fuck": 1},
+		},
+	}
+	knownSwears := loadKnownSwears("known_swears.txt")
+	for _, c := range cases {
+		if output := countSwears(knownSwears, c.input); output != c.expectedOutput {
+			t.Errorf("%s: got %d but expected %d", c.name, output, c.expectedOutput)
+		}
+	}
+}
+
+func TestLoadKnownSwears(t *testing.T) {
 
 }
 
-func TestloadKnownSwears(t *testing.T) {
+func TestLoadWords(t *testing.T) {
 
 }
 
-func TestloadWords(t *testing.T) {
-
-}
-
-func TestopenFile(t *testing.T) {
+func TestOpenFile(t *testing.T) {
 	// hint os.Args = []string{"cmd", "path/to/file.txt"}
 }
